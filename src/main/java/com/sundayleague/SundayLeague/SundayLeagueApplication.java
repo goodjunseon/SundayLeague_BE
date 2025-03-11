ab54bd6ab27899e82ebf2187ddca4e6e96ff9f15
goodjunseon <pzs2001926@gmail.com>
2025-03-11 19:29:40 +0900

    first commit

---
+++ src/main/java/com/sundayleague/SundayLeague/SundayLeagueApplication.java
@@ -0,0 +1,13 @@
+package com.sundayleague.SundayLeague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundayLeagueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundayLeagueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sundayleague/SundayLeague/SundayLeagueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sundayleague.SundayLeague;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SundayLeagueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
