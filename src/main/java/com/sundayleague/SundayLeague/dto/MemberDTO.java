bc19ef4de9667a2feb330f0bdfb3a702b99bb3d4
goodjunseon <pzs2001926@gmail.com>
2025-03-17 14:26:00 +0900

    회원 삭제, 로그아웃, 회원 정보 변경 기능 추가

--- src/main/java/com/sundayleague/SundayLeague/dto/MemberDTO.java
+++ src/main/java/com/sundayleague/SundayLeague/dto/MemberDTO.java
@@ -4,6 +4,8 @@ package com.sundayleague.SundayLeague.dto;
 import com.sundayleague.SundayLeague.entity.MemberEntity;
 import lombok.*;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @NoArgsConstructor // 생성자 생성 알아서 해줌, final 변수 그냥 선언 가능
@@ -16,7 +18,17 @@ public class MemberDTO {
     private String memberTel; // 전화번호
     private String memberDate; // 생년월일 YYYY-MM-DD
 
-//    public MemberEntity toMemberEntity(MemberDTO memberDTO) {
-//        return null;
-//    }
+    public static MemberDTO toMemberDTO(MemberEntity memberEntity) {
+
+        MemberDTO memberDTO = new MemberDTO();
+
+        memberDTO.setId(memberEntity.getId());
+        memberDTO.setMemberEmail(memberEntity.getMemberEmail());
+        memberDTO.setMemberPassword(memberEntity.getMemberPassword());
+        memberDTO.setMemberName(memberEntity.getMemberName());
+        memberDTO.setMemberTel(memberEntity.getMemberTel());
+        memberDTO.setMemberDate(memberEntity.getMemberDate());
+
+        return memberDTO;
+    }
 }
--- src/main/java/com/sundayleague/SundayLeague/repository/MemberRepository.java
+++ src/main/java/com/sundayleague/SundayLeague/repository/MemberRepository.java
@@ -4,8 +4,15 @@ import com.sundayleague.SundayLeague.entity.MemberEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional; // Option : Null 방지 클래스
+
 @Repository
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
  // JpaRepository<MemberEntity, Long> MemberEntity의 PK 자료형은 Long으로 설정한 것임.
+
+    // 이메일로 회원 정보 조회 (select * from member_table where member_email=?)
+    Optional <MemberEntity> findByMemberEmail(String memberEmail);
+
+
 
 }
